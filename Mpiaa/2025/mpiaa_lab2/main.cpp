d043e895c7bf105974f28ac922797e0083c59687
borov <justacancer4head@gmail.com>
2025-10-06 20:32:08 +0700

    mpiaa 2 commit 1

---
+++ Mpiaa/2025/mpiaa_lab2/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "lab2mpiaa.h"
+
+int main()
+{
+    Computing c;
+    double* vector_a = NULL,* vector_b = NULL;
+    c.initialize_vector(vector_a);
+    c.initialize_vector(vector_b);
+    c.print_vector(vector_a);
+    c.print_vector(vector_b);
+    std::cout << "Sequential: " << c.dot_product_sequential(vector_a, vector_b);
+    std::cout << "\nParallel: " << c.dot_product_parallel(vector_a, vector_b);
+}
