5d458f17775ad8573fe46da07cc9ea8fc18e1139
borov <justacancer4head@gmail.com>
2025-10-08 12:51:08 +0700

    mpiaa 2 vectors_task improved

--- Mpiaa/2025/mpiaa_lab2/vectors.cpp
+++ Mpiaa/2025/mpiaa_lab2/vectors.cpp
@@ -10,10 +10,10 @@ double dot_product_sequential(const int n, const double* vector_a, const double*
 	return result;
 }
 
-double dot_product_parallel(const int n, const double* vector_a, const double* vector_b)
+double dot_product_parallel(const int n, const double* vector_a, const double* vector_b, const int num_threads)
 {
 	double result = 0;
-#pragma omp parallel for reduction(+: result)
+#pragma omp parallel for reduction(+: result) num_threads(num_threads)
 	for (int i = 0; i < n; i++)
 	{
 		result += vector_a[i] * vector_b[i];
@@ -21,8 +21,9 @@ double dot_product_parallel(const int n, const double* vector_a, const double* v
 	return result;
 }
 
-void vectors_task(const int n)
+void vectors_task(const int n, const int num_threads)
 {
+	std::cout << std::fixed << std::setprecision(3);
 	double* vector_a = new double[n];
 	for (int i = 0; i < n; i++)
 		vector_a[i] = i % 10;
@@ -33,4 +34,12 @@ void vectors_task(const int n)
 	auto time = std::chrono::duration<double>(t2-t1).count();
 	std::cout << "* Sequential, N=" << n << " *\n";
 	std::cout << "Time elapsed: " << time << "\nResult: " << dot_product << "\n";
+
+	t1 = std::chrono::high_resolution_clock::now();
+	dot_product = dot_product_parallel(n, vector_a, vector_a, num_threads);
+	t2 = std::chrono::high_resolution_clock::now();
+	auto time_parallel = std::chrono::duration<double>(t2 - t1).count();
+	std::cout << "* Parallel, N=" << n << ", threads=" << num_threads << " *\n";
+	std::cout << "Time elapsed: " << time_parallel << "\nResult: " << dot_product << "\n";
+	std::cout << "Speedup: " << time / time_parallel << ", speedup/threads: " << time/time_parallel/num_threads << "\n";
 }
