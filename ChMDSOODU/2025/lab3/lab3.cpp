a24364436e9b19ad01a8f6747f0be527ca61a4fd
Evgenii Burov <evgeny8584261@gmail.com>
2025-10-18 14:20:22 +0700

    chmdsoodu 3 fixed simple implicit method

--- ChMDSOODU/2025/lab3/lab3.cpp
+++ ChMDSOODU/2025/lab3/lab3.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-	Equation1 eq(0, 1, 1);
-	std::vector<double> h_list({ 0.1,0.05, 0.025 });
-	Method m(h_list);
-	Solver<Equation1, Method> s(eq, m);
+	Equation2 eq(0, 2, 1);
+	std::vector<double> h_list({ 0.1, 0.05, 0.025 });
+	EulerImplicit m(h_list);
+	Solver<Equation2, EulerImplicit> s(eq, m);
 	s.solve("output.txt");
 }
--- ChMDSOODU/2025/lab3/solver_class.h
+++ ChMDSOODU/2025/lab3/solver_class.h
@@ -16,22 +16,24 @@ private:
 public:
 	Solver(Equation& equation0, Method& method0)
 		: equation(equation0), method(method0)
-	{ }
+	{
+	}
 
 	void solve(std::string file_name)
 	{
 		std::vector<double> h_list(method.get_h_list());
 		std::ofstream output_stream(file_name);
+		output_stream << std::scientific << std::setprecision(5) << "Method used: " << method.method_name();
 		std::vector<double> parameters = equation.get_parameters();
 		for (double h : h_list)
 		{
 			double t_start = parameters[0], t_end = parameters[1], y0 = parameters[2];
 
-			output_stream << "\nt\t\ty_num\t\ty_a\t\t|y_num-y_a|\n";
-			for (double t = t_start, y = y0; t < t_end || abs(t - t_end) < eps; t += h, y = method.get_next_y(t, y, h, equation))
+			output_stream << "\nt\ty_num\ty_a\t|y_num-y_a|\n";
+			for (double t = t_start, y = y0; t < t_end || abs(t - t_end) < eps; y = method.get_next_y(t, y, h, equation), t += h)
 			{
 				double y_a = equation.analytical_solution(t);
-				output_stream << t << "\t\t" << y << "\t\t" << y_a << "\t\t" << abs(y-y_a) << "\n";
+				output_stream << t << "\t" << y << "\t" << y_a << "\t" << abs(y - y_a) << "\n";
 			}
 		}
 		output_stream.close();
@@ -45,7 +47,8 @@ private:
 public:
 	Equation1(double t0, double t1, double y_0)
 		: t_start(t0), t_end(t1), y0(y_0)
-	{ }
+	{
+	}
 
 	std::vector<double> get_parameters()
 	{
@@ -63,24 +66,184 @@ public:
 	}
 };
 
-class Method
+class Equation2
 {
 private:
-	std::vector<double> h_list;
+	double t_start, t_end, y0;
+public:
+	Equation2(double t0, double t1, double y_0)
+		: t_start(t0), t_end(t1), y0(y_0)
+	{
+	}
 
+	std::vector<double> get_parameters()
+	{
+		return { t_start, t_end, y0 };
+	}
+
+	double f_of_ty(double t, double y)
+	{
+		return -25 * y + cos(t) + 25 * sin(t);
+	}
+
+	double analytical_solution(double t)
+	{
+		return exp(-25 * t) + sin(t);
+	}
+
+	double f_of_x_s(double x, double t, double y, double h)
+	{
+		return y - x * (25 * h + 1) + h * (cos(t + h) + sin(t + h));
+	}
+
+	double df_over_dx_s(double x, double t, double y, double h)
+	{
+		return -1*(25 * h + 1);
+	}
+
+	double f_of_x_t(double x, double t, double y, double h)
+	{
+		//x=y+(h/2)(f(t,y)+f(t+h,x))
+		//	x=y+(h/2)(-25y+cost+25sint-25x+cos(t+h)+25sin(t+h))
+		//	x=y-25hx/2+(h/2)(-25y + cost + 25sint + cos(t + h) + 25sin(t + h))
+		//	F(x)=y-x(1+25h/2) + (h / 2)(-25y + cost + 25sint + cos(t + h) + 25sin(t + h))
+		return y - x * (1 + 25 * h / 2) + h * (-25 * y + cos(t) + 25 * sin(t) + cos(t + h) + 25 * sin(t + h)) / 2;
+	}
+
+	double df_over_dx_t(double x, double t, double y, double h)
+	{
+		return -1*(1 + 25 * h / 2);
+	}
+};
+
+
+class Method
+{
+protected:
+	std::vector<double> h_list;
+	double eps = 1e-7;
 public:
 	Method(std::vector<double>& h)
 		: h_list(h)
-	{ }
+	{
+	}
 
 	std::vector<double> get_h_list()
 	{
 		return h_list;
 	}
 
+	virtual std::string method_name() const
+	{
+		return "DefaultMethod";
+	}
+
 	template<class Equation>
 	double get_next_y(double t, double y, double h, Equation& eq)
 	{
 		return y + h * eq.f_of_ty(t, y);
+	}
+};
+
+class EulerSimple : public Method
+{
+public:
+	using Method::Method;
+
+	std::string method_name() const override
+	{
+		return "EulerSimple";
+	}
+};
+
+class EulerImplicit : public Method
+{
+public:
+	using Method::Method;
+
+	std::string method_name() const override
+	{
+		return "EulerSimpleImplicit";
+	}
+
+	template<class Equation>
+	double get_next_y(double t, double y, double h, Equation& eq)
+	{
+		double x = 1;
+		double f_x = 1;
+		double df_dx = 1;
+		do
+		{
+			df_dx = eq.df_over_dx_s(x, t, y, h);
+			f_x = eq.f_of_x_s(x, t, y, h);
+			x -= f_x / df_dx;
+		} while (abs(f_x / df_dx) > eps);
+		return x;
+	}
+};
+
+class EulerTrapezoid : public Method
+{
+public:
+	using Method::Method;
+
+	std::string method_name() const override
+	{
+		return "EulerTrapezoid";
+	}
+
+	template<class Equation>
+	double get_next_y(double t, double y, double h, Equation& eq)
+	{
+		double x = 1;
+		double f_x = 1;
+		double df_dx = 1;
+		do
+		{
+			df_dx = eq.df_over_dx_t(x, t, y, h);
+			f_x = eq.f_of_x_t(x, t, y, h);
+			x -= f_x / df_dx;
+		} while (abs(f_x / df_dx) > eps);
+		return x;
+	}
+};
+
+class RungeKutta4 : public Method
+{
+public:
+	using Method::Method;
+
+	std::string method_name() const override
+	{
+		return "RungeKutta4";
+	}
+
+	template<class Equation>
+	double get_next_y(double t, double y, double h, Equation& eq)
+	{
+		double k, k1, k2, k3, k4;
+		k1 = eq.f_of_ty(t, y);
+		k2 = eq.f_of_ty(t + h / 2, y + h * k1 / 2);
+		k3 = eq.f_of_ty(t + h / 2, y + h * k2 / 2);
+		k4 = eq.f_of_ty(t + h, y + h * k3);
+		k = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		return y + h * k;
+	}
+};
+
+class EulerModified : public Method
+{
+public:
+	using Method::Method;
+
+	std::string method_name() const override
+	{
+		return "EulerModified";
+	}
+
+	template<class Equation>
+	double get_next_y(double t, double y, double h, Equation& eq)
+	{
+		return y + h * (eq.f_of_ty(t, y) + eq.f_of_ty(t + h, y + h * eq.f_of_ty(t, y))) / 2;
 	}
 };
