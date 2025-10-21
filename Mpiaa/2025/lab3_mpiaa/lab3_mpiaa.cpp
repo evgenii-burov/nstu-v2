5ba9f697603935ce13a0736a1fd7b33e3ae91fb9
borov <justacancer4head@gmail.com>
2025-10-21 14:29:25 +0700

    mpiaa 3 dynamic array fixed

--- Mpiaa/2025/lab3_mpiaa/lab3_mpiaa.cpp
+++ Mpiaa/2025/lab3_mpiaa/lab3_mpiaa.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-	stack_task(10);
-	priority_queue_task(10);
-	true_priority_queue_task(10);
-	dynamic_array_task(20, 3);
-	array_container_compatibility();
+	for (int i = 0; i < 20; i++)
+	{
+		//stack_task(10);
+		//priority_queue_task(10);
+		//true_priority_queue_task(10);
+		dynamic_array_task(20, 1);
+		//array_container_compatibility();
+	}
 }
