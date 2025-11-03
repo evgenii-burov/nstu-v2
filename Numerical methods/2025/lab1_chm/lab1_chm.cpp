d0aa581b49cfe89032fb7be240e564c8759bfbb1
Evgenii Burov <evgeny8584261@gmail.com>
2025-11-03 12:25:38 +0700

    chm 1 gauss precision and output fixed

--- Numerical methods/2025/lab1_chm/lab1_chm.cpp
+++ Numerical methods/2025/lab1_chm/lab1_chm.cpp
@@ -2,11 +2,10 @@
 
 int main()
 {
-    int k = 14;
+    int k = 10;
     LinearSystem ls("input.txt", 2, k);
     std::cout << "----k=" << k << "------\n";
     ls.print_ls();
     ls.solve_LDU();
     ls.compare_x();
-    ls.print_ls();
 }
