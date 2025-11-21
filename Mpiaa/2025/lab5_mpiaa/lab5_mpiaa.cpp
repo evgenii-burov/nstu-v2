9e7bad80e210ffeeae360859eb533b9ee73a157a
Evgenii Burov <evgeny8584261@gmail.com>
2025-11-21 18:06:51 +0700

    mpiaa 5 initial commit

---
+++ Mpiaa/2025/lab5_mpiaa/lab5_mpiaa.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv)
+{
+	//#1
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	std::cout << "\nSize: " << size;
+	std::cout << "\nRank: " << rank;
+
+	MPI_Finalize();
+	//getchar();
+	return 0;
+}
+
+
