66a9baa3846d2c013fa371efb71758aa04915cc8
borov <justacancer4head@gmail.com>
2026-02-20 14:19:18 +0700

    om 1 fixes

--- 2026/Optimization methods/Om_lab1/Om_lab1.cpp
+++ 2026/Optimization methods/Om_lab1/Om_lab1.cpp
@@ -3,5 +3,7 @@
 int main()
 {
     FunctionMinimization fm;
-    fm.dichotomy_method(0.000001);
+    double eps = 1e-8;
+    fm.find_interval_containing_minimum();
+    //fm.golden_ratio_method(eps);
 }
