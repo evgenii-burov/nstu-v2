4a006ffc09f6c99a928649ea4594ec6483f91b0a
Evgenii Burov <evgeny8584261@gmail.com>
2026-03-02 12:53:09 +0700

    TM 1 bub

--- 2026/Translation methods/TM_lab1/menu.cpp
+++ 2026/Translation methods/TM_lab1/menu.cpp
@@ -150,7 +150,7 @@ void dynamic_table_menu()
 			}
 			else
 			{
-				std::cout << "Incorrect size value\n";
+				std::cout << "Incorrect size value, re-enter the command to attempt a new resize\n";
 			}
 			break;
 		}
