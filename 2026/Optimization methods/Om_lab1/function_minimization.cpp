59b85876301bfacaa3ff5f887d706d57baeed8ed
Evgenii Burov <evgeny8584261@gmail.com>
2026-03-10 13:39:29 +0700

    om1 buggaga

--- 2026/Optimization methods/Om_lab1/function_minimization.cpp
+++ 2026/Optimization methods/Om_lab1/function_minimization.cpp
@@ -119,7 +119,7 @@ double FunctionMinimization::golden_ratio_method(double eps)
 
 void FunctionMinimization::find_interval_containing_minimum(double x0)
 {
-	bool failed = false;
+
 	double x = x0;
 	double delta = 1;
 	double h;
@@ -140,7 +140,6 @@ void FunctionMinimization::find_interval_containing_minimum(double x0)
 	}
 	while (func(x) > func(x + h))
 	{
-		//std::cout << "x: " << x << "\txh: " << x + h << '\n';
 		x += h;
 		h *= 2;
 	}
@@ -154,10 +153,6 @@ void FunctionMinimization::find_interval_containing_minimum(double x0)
 		b0 = x - h;
 		a0 = x + h;
 	}
-	if (a0 > 4 || b0 < 4)
-	{
-		std::cout << "Failed a0 " << a0 << " b0 " << b0 << " x " << x;
-		failed = true;
-	}
-	//std::cout << "a0: " << a0 << "\tb0: " << b0 << '\n';
+
+	std::cout << "a0: " << a0 << "\tb0: " << b0 << '\n';
 }
