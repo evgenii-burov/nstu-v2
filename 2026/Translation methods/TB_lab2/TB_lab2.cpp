652bcac676fe7e99cfef16097dfdaf85be02efdd
Evgenii Burov <evgeny8584261@gmail.com>
2026-04-13 12:39:07 +0700

    TM2 smth

--- 2026/Translation methods/TB_lab2/TB_lab2.cpp
+++ 2026/Translation methods/TB_lab2/TB_lab2.cpp
@@ -5,17 +5,7 @@
 using namespace std;
 int main()
 {
-    int i = 1;
-    switch (i) {
-    case 1:
-    {
-        cout << 1;
-    }
-    case 2:
-    {
-        cout << 2;
-    }
-    }
+	
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
