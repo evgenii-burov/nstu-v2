0bd03940dbf1d91b5ed4ba03582c4c8f97e27d1e
borov <justacancer4head@gmail.com>
2026-04-19 20:19:52 +0700

    om 2 minor touch ups

--- 2026/Optimization methods/Om_lab2/Om_lab2.cpp
+++ 2026/Optimization methods/Om_lab2/Om_lab2.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
     cout << fixed << setprecision(6);
     QuadraticFunction quadratic;
-    double eps_f = 1e-7;
-    double eps_x = 1e-7;
+    double eps_f = 1e-3;
+    double eps_x = 1e-3;
     HookJeeves hj(quadratic, eps_f, eps_x);
     GradientDescent gd(quadratic, eps_f, eps_x);
     gd.minimize(Point(5, 1), true);
