a50a56e000d4c4ce0d118966ed3b9e88bd1cff63
Evgenii Burov <evgeny8584261@gmail.com>
2026-04-20 22:20:41 +0700

    om 2 iterations added to verbose output

--- 2026/Optimization methods/Om_lab2/Om_lab2.cpp
+++ 2026/Optimization methods/Om_lab2/Om_lab2.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main() {
-    cout << fixed << setprecision(6);
+    cout << fixed << setprecision(7);
     QuadraticFunction quadratic;
-    double eps_f = 1e-3;
-    double eps_x = 1e-3;
-    HookJeeves hj(quadratic, eps_f, eps_x);
-    GradientDescent gd(quadratic, eps_f, eps_x);
-    gd.minimize(Point(5, 1), true);
+    Rosenbrock rosenbrock;
+    Variant4 v;
+    double eps = 1e-3;
+    double eps_f = eps;
+    double eps_x = eps;
+    HookJeeves hj(v, eps_f, eps_x);
+    GradientDescent gd(v, eps_f, eps_x);
+    gd.minimize(Point(2.5, 10), true);
 }
