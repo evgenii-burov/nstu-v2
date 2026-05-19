4aac06df49b0e7559a6a85019c3f9a79690ac523
borov <justacancer4head@gmail.com>
2026-05-19 14:50:25 +0700

    Om python visualizaiton, variant4 function

--- 2026/Optimization methods/om_python/main.py
+++ 2026/Optimization methods/om_python/main.py
@@ -1,6 +1,14 @@
 from OneDimensionalMinimization import *
+from TwoDimensionalMinimization import *
 
-q = Quadratic1DFunction()
-gr = GoldenRatio(q, -10,10,1e-7)
+q = QuadraticFunction()
+r = RosenbrockFunction()
+v = Variant4Function()
 
-print(gr())
+eps = 1e-7
+start = Point(-10, 10)
+
+hj = HookJeeves(v, start, eps, eps)
+
+if __name__ == '__main__':
+    hj(3)
